a4f05925878bca0c2a731a049b772e03a406e954
Mayanka-88 <70584944+Mayanka-88@users.noreply.github.com>
2021-05-28 14:14:22 +0530

    Updated

--- src/Recipe.js
+++ src/Recipe.js
@@ -5,6 +5,11 @@ const Recipe = ({title, calories, image, ingredients}) => {
     return (
         <div>
             <h1>{title}</h1>
+            <ol>
+                {ingredients.map(ingredients => {
+                    <li>{ingredients.text}</li>
+                })}
+            </ol>
             <p>{calories}</p>
             <img src={image} />
         </div>
