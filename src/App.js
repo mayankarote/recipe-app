7f82437ccd7cddefa1ea105a73361b9dda666913
Mayanka-88 <70584944+Mayanka-88@users.noreply.github.com>
2021-05-28 12:34:04 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect , useState} from 'react';
 import './App.css';
 
 const App = () => {
   const APP_ID = "087d0d95";
-  const APP_Key = "ba7ebf6b028795b0f6f1500387926d6c	";
-
+  const APP_KEY = "ba7ebf6b028795b0f6f1500387926d6c	";
   const exaampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`;
+
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    console.log('Effect has been run ');
+  });
 
   return (
     
     <div className="App">
         <form className="search-form">
         <input className="search-bar" type="text" />
-        <button className="search-button" type="submit">Search</button>
+        <button onClick={() =>setCounter(counter + 1)} className="search-button" type="submit">Search</button>
         </form>
     </div>
   );
